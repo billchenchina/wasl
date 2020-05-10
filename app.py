74d8f1ac0b50e9617b433afa783c56c0487dced9
billchenchina <billchenchina2001@gmail.com>
2020-05-10 21:05:35 +0800

    Add all

--- app.py
+++ app.py
@@ -10,9 +10,12 @@ db = SQLAlchemy(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
 @app.route('/login/')
 def login():
     return render_template('login.html')
+
 @app.route('/register/')
 def register():
     return render_template('register.html')
